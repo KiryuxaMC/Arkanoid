a4d3727323b8165cb596d916694cf5f2902f7440
My Name <mckiryuxa@mail.ru>
2020-01-30 19:07:17 +0700

    Незначительные изменения

--- main.py
+++ main.py
@@ -1,26 +1,31 @@
 import pygame
+import game_functions as gf
 from settings import Settings
+from board import Board
+
 
 def main():
     # Подключение класса настроек
     sett = Settings()
+
+    #Подключение класса доски
+    board = Board([sett.W // 2 - 50, sett.H - 40])
 
     # Инициализация pygame, настройка экрана
     pygame.init()
     screen = pygame.display.set_mode((sett.W, sett.H))
     pygame.display.set_caption('Arkanoid')
 
-    # Переменная времени
-    clock = pygame.time.Clock()
-    pygame.display.update()
+
+    
 
     while True:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                exit()
+        # Обработка событий
+        gf.check_events()
         
-        pygame.display.update()
-        screen.fill(sett.white)
+        #Обновление экрана
+        gf.update_screen(sett, screen, board)
+        
 
 if __name__ == '__main__':
     main()
