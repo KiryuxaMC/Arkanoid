c8a86a61cc267eb097b65b906463718b5e0e66a9
My Name <mckiryuxa@mail.ru>
2020-02-05 15:44:01 +0700

    Внёс изминения

---
+++ game_stats.py
@@ -0,0 +1,8 @@
+class GameStats:
+    '''Класс игровой статистики'''
+    def __init__(self):
+        '''Инициализирует статистику'''
+        self.score = 0
+        self.game_active = False
+
+    
