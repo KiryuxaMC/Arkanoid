223dbe585407416d61067cd211e20c3d5098526e
My Name <mckiryuxa@mail.ru>
2020-02-05 15:44:10 +0700

    Внёс изминения

--- main.py
+++ main.py
@@ -3,11 +3,17 @@ import game_functions as gf
 from settings import Settings
 from board import Board
 from ball import Ball
+from button import Button
+from game_stats import GameStats
+from bg import BG
 from pygame.sprite import Group
 
 def main():
     # Подключение класса настроек
     sett = Settings()
+
+    # Подключение класса статистики
+    stats = GameStats()
 
     # Инициализация pygame, настройка экрана
     pygame.init()
@@ -24,9 +30,16 @@ def main():
     # Подключение класса мячика
     ball = Ball(screen, sett, board)
 
+    # Создание кнопки Play
+    play_butt = Button(sett, screen, 'Play')
+
+    # Подключение класса фонового изображения
+    bg = BG(screen)
+
+
     while True:
         # Обработка событий
-        gf.check_events(sett, board)
+        gf.check_events(sett, board, stats, play_butt, screen, bricks)
         
         # Обновлений позиций доски
         board.update()
@@ -35,10 +48,16 @@ def main():
         ball.update()
 
         # Проверка коллизий с обьектами
-        gf.check_colision(sett, board, ball, bricks)
+        gf.check_colision(sett, board, ball, bricks, stats)
+        
+        # Проверка конца игы
+        gf.check_end_game(screen, sett, bricks, ball, stats)
 
-        #Обновление экрана
-        gf.update_screen(sett, screen, board, ball, bricks)
+        # Обновление экрана
+        gf.update_screen(sett, screen, board, ball, bricks, play_butt, 
+                                                            stats,
+                                                            bg)
+        
         
 
 if __name__ == '__main__':
