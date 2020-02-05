a9298e599da1e602e9fd8352dc2cea4fe4150d8e
My Name <mckiryuxa@mail.ru>
2020-02-05 15:42:31 +0700

    Внёс изминения

--- brick.py
+++ brick.py
@@ -1,5 +1,6 @@
 import pygame
 from pygame.sprite import Sprite
+from random import randint as rd
 
 class Brick(Sprite):
     '''Класс кирпича'''
@@ -9,7 +10,7 @@ class Brick(Sprite):
         self.screen = screen
         self.screen_rect = screen.get_rect()
         self.settings = sett
-        self.image = pygame.image.load('Arkanoid_Im/block01.png')
+        self.image = pygame.image.load(f'Arkanoid_Im/block0{rd(1, 5)}.png')
         self.rect = self.image.get_rect()
         self.rect.x = self.rect.width
         self.rect.y = self.rect.height
